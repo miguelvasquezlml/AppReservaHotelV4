fb1242f6fa82186dd073ba87b1450cb7c27600b7
ravilaUPC21 <u201000444@upc.edu.pe>
2023-07-09 22:26:06 -0500

    Merge remote-tracking branch 'origin/main'

---
+++ app/src/main/java/com/upc/appreservahotelv4/RegistraReservaActivity.java
@@ -0,0 +1,14 @@
+package com.upc.appreservahotelv4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistraReservaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registra_reserva);
+    }
+}
